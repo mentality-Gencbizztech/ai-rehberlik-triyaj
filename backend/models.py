0eff8b223ef33641ec814bfcdf5ba111dbc9fb04
mentality-Gencbizztech <mentalitygencbizztech@gmail.com>
2026-02-09 17:35:14 +0300

    Update and rename models.txt to models.py

---
+++ backend/models.py
@@ -0,0 +1,7 @@
+fake_users_db = {
+    "rehber1": {
+        "username": "rehber1",
+        "hashed_password": "$2b$12$KIXQkzXg1nq9pOZQ1dRj5uQ8q1qY5p4Yw5vXGzPp9Xy",
+        "role": "counselor"
+    }
+}
